ddfacfe2a73ec856cb823e32906d7429d7a7da25
zDefcon20 <antholozadarequelme@gmail.com>
2025-02-03 16:22:46 -0500

    Eliminar archivo de configuración de Visual Studio y actualizar rutas de proyecto en archivos de configuración generados

--- Csharp_Ejercicio1/obj/Debug/net9.0/Csharp_Ejercicio1.AssemblyInfo.cs
+++ Csharp_Ejercicio1/obj/Debug/net9.0/Csharp_Ejercicio1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp_Ejercicio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a6654e11154f424edd74dab34b98d1aeedac7bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+113bbd7cf9850a0b5dfe49aaa79de232f3b72a2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp_Ejercicio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp_Ejercicio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- EjercicioNivelacion/Program.cs
+++ EjercicioNivelacion/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace EjercicioNivelacion
+{
+  class Program
+  {
+    static void main(string[] args)
+    {
+      string op = "si";
+      int Menu, respuesta;
+      string[] Alumno = new string[5];
+      double[,] Nota = new double[5, 2];
+
+      while (op == "si")
+      {
+        Console.WriteLine("Programa para notas de 5 alumnos");
+        Console.WriteLine("Escoja una opcion del menu");
+        Console.WriteLine("1: Ingresar Informacion");
+        Console.WriteLine("2: Mostrar Notas Promedias");
+        Console.WriteLine("3: Buscar Alumno y saber si aprobo");
+        Menu = Convert.ToInt32(Console.ReadLine());
+
+        switch (Menu)
+        {
+          case 1:
+
+            for (int i = 0; i < 5; i++)
+            {
+              Console.WriteLine($"Ingrese el nombre del alumno# {i + 1}");
+              Alumno[i] = Console.ReadLine();
+              for (int j = 0; j < 2; j++)
+              {
+                if (j == 0)
+                {
+                  Console.WriteLine("Ingrese la nota del primer parcial");
+                  Nota[i, j] = Convert.ToDouble(Console.ReadLine());
+                }
+                else
+                {
+                  Console.WriteLine("Ingrese la nota del segundo parcial");
+                  Nota[i, j] = Convert.ToDouble(Console.ReadLine());
+                }
+                Console.WriteLine("La nota de los 5 alumnos fue ingresada correctamente");
+              }
+            }
+            break;
+
+            case 2:
+            for(int i = 0; i < 5; i++)
+            {
+               Console.WriteLine($"La nota promedio del alumno {Alumno[i]} es = {(Nota[i,0]+Nota[i,1])/2}"); 
+            }
+            break;
+        }
+      }
+
+    }
+  }
+}

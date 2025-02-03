a7fe8b7835486ba51951eaf4e03dfaab5b4d0bc1
zDefcon20 <antholozadarequelme@gmail.com>
2025-02-03 16:46:59 -0500

    Agregar archivos de configuración y caché para los proyectos Ejercicio5 y Ejercicio_7, y actualizar archivos de configuración para Csharp_Fundaments y EjercicioNivelacion

--- EjercicioNivelacion/Program.cs
+++ EjercicioNivelacion/Program.cs
@@ -5,7 +5,7 @@ namespace EjercicioNivelacion
 {
   class Program
   {
-    static void main(string[] args)
+    static void Main(string[] args)
     {
       string op = "si";
       int Menu, respuesta;
@@ -14,6 +14,7 @@ namespace EjercicioNivelacion
 
       while (op == "si")
       {
+        Console.WriteLine("----Bienvenido al programa----");
         Console.WriteLine("Programa para notas de 5 alumnos");
         Console.WriteLine("Escoja una opcion del menu");
         Console.WriteLine("1: Ingresar Informacion");
@@ -41,18 +42,71 @@ namespace EjercicioNivelacion
                   Console.WriteLine("Ingrese la nota del segundo parcial");
                   Nota[i, j] = Convert.ToDouble(Console.ReadLine());
                 }
-                Console.WriteLine("La nota de los 5 alumnos fue ingresada correctamente");
               }
+              Console.WriteLine("La nota de los 5 alumnos fue ingresada correctamente");
             }
             break;
 
-            case 2:
-            for(int i = 0; i < 5; i++)
+          case 2:
+            for (int i = 0; i < 5; i++)
             {
-               Console.WriteLine($"La nota promedio del alumno {Alumno[i]} es = {(Nota[i,0]+Nota[i,1])/2}"); 
+              Console.WriteLine($"La nota promedio del alumno {Alumno[i]} es = {(Nota[i, 0] + Nota[i, 1]) / 2}");
             }
             break;
+
+          case 3:
+            Console.WriteLine("Ingrese el numero del alumno que desea buscar");
+            Console.WriteLine($"Ingrese: 1 para buscar el promedio del alumno: {Alumno[0]}");
+            Console.WriteLine($"Ingrese: 2 para buscar el promedio del alumno: {Alumno[1]}");
+            Console.WriteLine($"Ingrese: 3 para buscar el promedio del alumno: {Alumno[2]}");
+            Console.WriteLine($"Ingrese: 4 para buscar el promedio del alumno: {Alumno[3]}");
+            Console.WriteLine($"Ingrese: 5 para buscar el promedio del alumno: {Alumno[4]}");
+
+            respuesta = Convert.ToInt32(Console.ReadLine());
+
+            if (respuesta == 1)
+            {
+              Console.WriteLine($"El promedio del alumno {Alumno[0]} es: {(Nota[0, 0] + Nota[0, 1]) / 2}");
+            }
+            else if (respuesta == 2)
+            {
+              Console.WriteLine($"El promedio del alumno {Alumno[1]} es: {(Nota[1, 0] + Nota[1, 1]) / 2}");
+            }
+            else if (respuesta == 3)
+            {
+              Console.WriteLine($"El promedio del alumno {Alumno[2]} es: {(Nota[2, 0] + Nota[2, 1]) / 2}");
+            }
+            else if (respuesta == 4)
+            {
+              Console.WriteLine($"El promedio del alumno {Alumno[3]} es: {(Nota[3, 0] + Nota[3, 1]) / 2}");
+            }
+            else if (respuesta == 5)
+            {
+              Console.WriteLine($"El promedio del alumno {Alumno[4]} es: {(Nota[4, 0] + Nota[4, 1]) / 2}");
+            }
+            else
+            {
+              Console.WriteLine("El numero ingresado no es valido");
+            }
+            break;
+
+          default:
+            Console.WriteLine("La opcion ingresada no es valida");
+            break;
+
         }
+      }
+      Console.WriteLine("Desea volver al Menu Principal? si/no");
+      Console.WriteLine("Ingrese si o no");
+
+      op = Console.ReadLine();
+      if(op == "si" || op == "SI" || op == "Si" || op == "sI")
+      {
+        op = "si";
+      }
+      else
+      {
+        Console.WriteLine("Gracias por usar el programa");
       }
 
     }

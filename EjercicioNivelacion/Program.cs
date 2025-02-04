79b924c208ef68fc2d0bce594fbebb27edf29c40
zDefcon20 <antholozadarequelme@gmail.com>
2025-02-03 19:51:24 -0500

    Actualizar archivos de configuración y caché para Ejercicio4, Ejercicio5, Ejercicio_6.0, Ejercicio_7 y otros proyectos

--- EjercicioNivelacion/Program.cs
+++ EjercicioNivelacion/Program.cs
@@ -8,6 +8,7 @@ namespace EjercicioNivelacion
     static void Main(string[] args)
     {
       string op = "si";
+      bool datosIngresados = false;
       int Menu, respuesta;
       string[] Alumno = new string[5];
       double[,] Nota = new double[5, 2];
@@ -24,37 +25,69 @@ namespace EjercicioNivelacion
 
         switch (Menu)
         {
-          case 1:
 
+        case 1:
+        if (!datosIngresados)
+        {
+            // Solicitar información de alumnos y notas
+            // Ejemplo:
+            Console.WriteLine("Ingrese la cantidad de alumnos a ingresar:");
             for (int i = 0; i < 5; i++)
             {
-              Console.WriteLine($"Ingrese el nombre del alumno# {i + 1}");
-              Alumno[i] = Console.ReadLine();
-              for (int j = 0; j < 2; j++)
-              {
-                if (j == 0)
-                {
-                  Console.WriteLine("Ingrese la nota del primer parcial");
-                  Nota[i, j] = Convert.ToDouble(Console.ReadLine());
-                }
-                else
-                {
-                  Console.WriteLine("Ingrese la nota del segundo parcial");
-                  Nota[i, j] = Convert.ToDouble(Console.ReadLine());
-                }
-              }
-              Console.WriteLine("La nota de los 5 alumnos fue ingresada correctamente");
-            }
-            break;
+                Console.WriteLine($"Ingrese el nombre para el alumno {i + 1}:");
+                Alumno[i] = Console.ReadLine();
 
-          case 2:
+                Console.WriteLine($"Ingrese la primera nota para el alumno {i + 1}:");
+                Nota[i, 0] = double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Ingrese la segunda nota para el alumno {i + 1}:");
+                Nota[i, 1] = double.Parse(Console.ReadLine());
+            }
+
+            datosIngresados = true;
+        }
+        else
+        {
+            Console.WriteLine("La información de los alumnos ya fue ingresada.");
+        }
+        Console.WriteLine("");
+        Console.WriteLine("Desea volver al Menu Principal? si/no");
+        Console.WriteLine("Ingrese si o no");
+
+        op = Console.ReadLine();
+        if(op == "si" || op == "SI" || op == "Si" || op == "sI")
+        {
+          op = "si";
+        }
+        else
+        {
+          Console.WriteLine("Gracias por usar el programa");
+        }
+        break;
+
+        case 2:
             for (int i = 0; i < 5; i++)
             {
               Console.WriteLine($"La nota promedio del alumno {Alumno[i]} es = {(Nota[i, 0] + Nota[i, 1]) / 2}");
             }
+            Console.WriteLine("");
+            Console.WriteLine("Desea volver al Menu Principal? si/no");
+            Console.WriteLine("Ingrese si o no");
+
+            op = Console.ReadLine();
+            if(op == "si" || op == "SI" || op == "Si" || op == "sI")
+            {
+              op = "si";
+            }
+            else
+            {
+              Console.WriteLine("Gracias por usar el programa");
+            }
+              
             break;
 
-          case 3:
+        case 3:
+        
             Console.WriteLine("Ingrese el numero del alumno que desea buscar");
             Console.WriteLine($"Ingrese: 1 para buscar el promedio del alumno: {Alumno[0]}");
             Console.WriteLine($"Ingrese: 2 para buscar el promedio del alumno: {Alumno[1]}");
@@ -66,23 +99,29 @@ namespace EjercicioNivelacion
 
             if (respuesta == 1)
             {
-              Console.WriteLine($"El promedio del alumno {Alumno[0]} es: {(Nota[0, 0] + Nota[0, 1]) / 2}");
+              double promedio = (Nota[0, 0] + Nota[0, 1]) / 2;
+              Console.WriteLine($"El promedio del alumno {Alumno[0]} es: {promedio} ");
             }
             else if (respuesta == 2)
             {
-              Console.WriteLine($"El promedio del alumno {Alumno[1]} es: {(Nota[1, 0] + Nota[1, 1]) / 2}");
+              double promedio = (Nota[1, 0] + Nota[1, 1]) / 2;
+              Console.WriteLine($"El promedio del alumno {Alumno[1]} es: {promedio}");
             }
             else if (respuesta == 3)
             {
-              Console.WriteLine($"El promedio del alumno {Alumno[2]} es: {(Nota[2, 0] + Nota[2, 1]) / 2}");
+              double promedio = (Nota[2, 0] + Nota[2, 1]) / 2;
+              Console.WriteLine($"El promedio del alumno {Alumno[2]} es: {promedio}");
             }
             else if (respuesta == 4)
             {
-              Console.WriteLine($"El promedio del alumno {Alumno[3]} es: {(Nota[3, 0] + Nota[3, 1]) / 2}");
+              double promedio = (Nota[3, 0] + Nota[3, 1]) / 2;
+              Console.WriteLine($"El promedio del alumno {Alumno[3]} es: {promedio}");
             }
             else if (respuesta == 5)
             {
-              Console.WriteLine($"El promedio del alumno {Alumno[4]} es: {(Nota[4, 0] + Nota[4, 1]) / 2}");
+              double promedio = (Nota[4, 0] + Nota[4, 1]) / 2;
+              Console.WriteLine($"El promedio del alumno {Alumno[4]} es: {promedio}");
+
             }
             else
             {

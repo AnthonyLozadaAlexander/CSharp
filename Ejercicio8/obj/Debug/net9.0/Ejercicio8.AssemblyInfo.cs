63932394e19a215aa73642761b4d93eb37a995e5
zDefcon20 <antholozadarequelme@gmail.com>
2025-02-06 18:32:56 -0500

    Eliminar archivos binarios y de configuración obsoletos de EjercicioVector, Ejercicio4 y Ejercicio5; agregar archivo de programa para Ejercicio9

--- Ejercicio8/obj/Debug/net9.0/Ejercicio8.AssemblyInfo.cs
+++ Ejercicio8/obj/Debug/net9.0/Ejercicio8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b655b623c0c57b1af84511a01489d872e798aa5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca7eb9ce2e8bb3bbc44011149b5794081f1b45c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercicio8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
